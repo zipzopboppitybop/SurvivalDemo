f0845ef25298e2d5d99bdf9d6e9ba27dcd07f9ae
Brian Washington Jr <bw49078@gmail.com>
2025-10-19 21:34:31 -0500

    Player can now crouch

--- SurvivalDemo/Assets/Scripts/PlayerController.cs
+++ SurvivalDemo/Assets/Scripts/PlayerController.cs
@@ -1,25 +1,37 @@
+using System.Diagnostics;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] CharacterController controller;
+    [SerializeField] Camera playerCamera;
     [SerializeField] float speed;
     [SerializeField] float sprintMod;
+    [SerializeField] float crouchMod;
+    [SerializeField] float cameraCrouchSpeed;
     [SerializeField] int jumpSpeed;
     [SerializeField] int jumpCountMax;
     [SerializeField] int gravity;
 
     private Vector3 moveDir;
     private Vector3 playerVel;
+    private Vector3 playerOriginalCenter;
+    private Vector3 playerCrouchingCenter;
+    private Vector3 cameraOriginalPos;
+    private Vector3 cameraCrouchingPos;
 
     bool isSprinting;
+    bool isCrouching;
 
     int jumpCount;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        playerOriginalCenter = controller.center;
+        playerCrouchingCenter = new Vector3(0, -.5f, 0);
+        cameraOriginalPos = playerCamera.transform.localPosition;
+        cameraCrouchingPos = cameraOriginalPos - new Vector3(0, 0.5f, 0);
     }
 
     // Update is called once per frame
@@ -28,6 +40,8 @@ public class PlayerController : MonoBehaviour
         Movement();
 
         Sprint();
+
+        Crouch();
     }
 
     void Movement()
@@ -71,6 +85,26 @@ public class PlayerController : MonoBehaviour
         {
             playerVel.y = jumpSpeed;
             jumpCount++;
+        }
+    }
+
+    void Crouch()
+    {
+        if (Input.GetButtonDown("Crouch"))
+        {
+            isCrouching = true;
+            speed /= crouchMod;
+            controller.height /= 2;
+            controller.center = playerCrouchingCenter; 
+            playerCamera.transform.localPosition = cameraCrouchingPos;
+        }
+        else if (Input.GetButtonUp("Crouch"))
+        {
+            isCrouching = false;
+            speed *= crouchMod;
+            controller.height *= 2;
+            controller.center = playerOriginalCenter;
+            playerCamera.transform.localPosition = cameraOriginalPos;
         }
     }
 }
